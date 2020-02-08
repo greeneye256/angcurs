42341b5db85aca5446b5b2886e0cff83f2b8c0d7
Radu Simu <radusimu@gmail.com>
2020-02-08 09:10:02 +0200

    new ver

---
+++ src/app/components/dog/dog.component.ts
@@ -0,0 +1,56 @@
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-dog',
+  templateUrl: './dog.component.html',
+  styleUrls: ['./dog.component.css']
+})
+export class DogComponent implements OnInit {
+
+  constructor() {
+  }
+
+  isActive = true;
+  buttonTitle = 'Hide me!';
+  warning = false;
+  message = 'Please enter a value!';
+
+  @Input() items;
+  @Input() itemtype;
+
+  selectedAnimal = '';
+
+  ngOnInit() {
+    console.log('Merge!');
+  }
+
+  changeState() {
+    this.isActive = !this.isActive;
+    this.buttonTitle = (this.isActive ? 'Hide me!' : 'Show me!');
+  }
+
+  getText(event) {
+    this.selectedAnimal = event.target.value;
+  }
+
+  isLengthEnough() {
+    if ((this.items.length < 3) && this.isActive) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  deleteItem() {
+    this.items.pop();
+  }
+
+  enterAnimal(s: string) {
+    if (s === '') {
+      this.warning = true;
+    } else {
+      this.items.push({id: this.items.length + 1, name: s});
+      this.warning = false;
+    }
+  }
+}
